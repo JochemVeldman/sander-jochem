1fe8e72887fbc0cf563584a510373e340723fb1e
Unknown <jochemveldman@hotmail.com>
2017-07-15 15:25:39 +0200

    reactie op vragen blok veranderd

--- vraag.php
+++ vraag.php
@@ -1,5 +1,6 @@
 <html>
 <!--goede versie -->
+
 <head>
     <?php 
             include_once('functions/mainfunctions.php');
@@ -32,7 +33,7 @@
         
             $conn = null;
         ?>
-        <?php
+    <?php
             if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['submit_reactie'])){
                 
                 $conn = connectDB();
@@ -67,25 +68,44 @@
 
         <div class="row" style="padding-right: 15px">
             <div class="col-md-6" style="background-color: white;">
-                    <?php
+                <?php
                         if ($melding == true){
                             echo '<p class="bg-success" style="padding: 15px; font-family: Montserrat; text-align: center; margin-bottom: 15px; border-radius: 3px">U reactie is met succes geplaatst.</p>';
                         }
                     ?>
-                <form method="POST" action="">
-                    <div class="form-group">
-                        <textarea placeholder="Plaats reactie" type="text" class="form-control" id="plaats_reactie" name="plaats_reactie" onInput="check_reactie()"></textarea>
-                    </div>
-                    <button type="submit" class="btn btn-default" id="submit_reactie" name="submit_reactie" disabled>Plaats reactie</button>
-                </form>
-                <br>
-                <?php
+                    <?php 
+                    if(is_loggedin()){
+                        echo '
+                        <form method="POST" action="">
+                            <div class="form-group">
+                                <textarea placeholder="Plaats reactie" type="text" class="form-control" id="plaats_reactie" name="plaats_reactie" onInput="check_reactie()"></textarea>
+                            </div>
+                            <button type="submit" class="btn btn-default" id="submit_reactie" name="submit_reactie" disabled>Plaats reactie</button>
+                        </form>
+                        <br>';
+                    }else {
+                        echo '<div class="container">
+                        <div class="col-md-5" style="background-color: #eee; padding: 20px; margin-bottom: 15px; margin-left: -15px;">
+                            <p><b>Om een reactie te plaatsen moet u inloggen of <a href="registreren.php">registreren</a></b></p>
+                            <form method="POST" action="">
+                                <div class="form-group">
+                                    <input type="text" class="form-control" placeholder="Gebruikersnaam/e-mail" id="login_gebruikersnaam" name="login_gebruikersnaam" required style="margin-top: 3px">
+                                    <input type="password" class="form-control" placeholder="Wachtwoord" id="login_wachtwoord" name="login_wachtwoord" required style="margin-top: 3px">
+                                    <button type="submit" class="btn btn-success" name="loginButton" style="margin-top: 3px">Login</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>';
+                    }
+                ?>
+                
+                    <?php
                     try{
                         $conn = connectDB();          
                         $sql = 'SELECT * FROM reacties INNER JOIN gebruikers ON reacties.gebruiker_id = gebruikers.id WHERE vraag_id =' . $_GET['id'] . ' ORDER BY reacties.reactie_id DESC';
                 
                         foreach ($conn->query($sql) as $row) {
-                            echo '<div class="vraag_blok"><p style= "font-family: Montserrat;">' . $row['reactie'] . '</p></div>';
+                            echo '<div class="col-md-5" id="vraag_blok><p style= "font-family: Montserrat;">' . $row['reactie'] . '</p></div>';
                         }
                     }
                     catch(PDOException $e){
@@ -97,7 +117,7 @@
             <div class="col-md-4 col-md-offset-2" style="background-color: #eee; padding: 20px;">
                 Gevraagd door:
                 <?php echo '<a href="gebruikers.php?id=3">'.$row['gebruikersnaam']. '</a><br>';?>
-                <?php echo $row['bekeken']; ?> keer bekeken sinds
+                <?php echo $row['bekeken']; ?> keer bekeken sinds 
                 <?php echo $row['datum']; ?>
             </div>
         </div>
