814f31b8fcf5301195632ee18df960f66c09895f
Unknown <jochemveldman@hotmail.com>
2017-07-18 00:55:51 +0200

    mooi geweest voor deze avond

--- vraag.php
+++ vraag.php
@@ -108,7 +108,7 @@
                         $sql = 'SELECT * FROM reacties INNER JOIN gebruikers ON reacties.gebruiker_id = gebruikers.id WHERE vraag_id =' . $_GET['id'] . ' ORDER BY reacties.reactie_id DESC';
                 
                         foreach ($conn->query($sql) as $row) {
-                            echo '<div class="reactie_blok"><p style= "font-family: Montserrat;">' . $row['reactie'] . '</p></div>';
+                            echo '<div class="reactie_blok"><p style= "font-family: Montserrat; font-size: 14px;">' . $row['reactie'] . '</p>' . '<p style="font-family: Montserrat; font-size: 12px; padding-bottom: 7px;">' . $row['gebruikersnaam'] . '</p>' . '</div>';
                         }
                     }
                     catch(PDOException $e){
